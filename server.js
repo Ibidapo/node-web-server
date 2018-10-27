f2eb0bb64745d86982f548a44ecb2b22ae802928
Ibidapo <dapsined@gmail.com>
2018-10-27 17:05:33 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,56 @@
+let express = require('express');
+let hbs = require('hbs');
+let fs = require('fs');
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let logInfo = `Time stamp: ${now}, method: ${req.method}, url:${req.originalUrl}`
+  console.log();
+  fs.appendFileSync('server.log', logInfo + '\n')
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs', {
+    pageTitle: 'Maintenance Page',
+    maintenanceMsg: 'This site is currently under maintenance. Please check back later'
+  });
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+}); 
+
+hbs.registerHelper('screamIt', (str) => {
+  return str.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMsg: 'Welcome to my **node-web-server** tutorial exercise ;)'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    aboutMsg: 'Welcome to the about section of **node-web-server** tutorial exercise ;)'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to fulfill this request'
+  })
+})
+
+app.listen(3000, () => {
+  console.log('Server is up and running on port 3000');
+});
